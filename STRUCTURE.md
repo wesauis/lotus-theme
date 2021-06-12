88d5fc4fba322907a92439fe19d9f21aa8241e88
wesauis <54046750+wesauis@users.noreply.github.com>
2021-06-12 19:19:17 -0300

    basic vscode theme

---
+++ STRUCTURE.md
@@ -0,0 +1,21 @@
+# Project Structure
+
+## Why
+
+The Lotus Theme project aims to make the theme available to a variety of software, for that this project will be structured to allow these multiple targets.
+
+**_This will be a monorepo :)_**
+
+## Structure
+
+- `schema/`: colors+ for the templates
+
+- `template/`: theme template
+
+- `assembler/`: code that will asseble the templates in build/dist
+
+- `dist/`: packed theme
+
+- `build/`: unpacked/assembled theme or some schemas to a future dist
+
+- `CHANGELOG.md`: [global changelog](http://keepachangelog.com/)
---
+++ template/vscode/package.js
@@ -0,0 +1,26 @@
+export default (schemas) => [
+  [
+    "package.json",
+    {
+      name: "lotus-theme",
+      displayName: "Lotus Theme",
+      description: "",
+      version: "0.0.1",
+      engines: {
+        vscode: "^1.57.0",
+      },
+      categories: ["Themes"],
+      contributes: {
+        themes: schemas.map((schema) => ({
+          label: `Lotus ${schema.name}`,
+          uiTheme: {
+            dark: "vs-dark",
+            light: "vs",
+            hc: "hc-black",
+          }[schema.lightness],
+          path: `./themes/lotus-${schema.name}.json`.toLowerCase(),
+        })),
+      },
+    },
+  ],
+];
